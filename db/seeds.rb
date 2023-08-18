b98f16d9156d581b2c44ac9e3da2628449437d71
Antonio Quaresma <tozequa@gmail.com>
2023-08-18 13:02:31 +0100

    heruku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+response = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
+movies_data = JSON.parse(response)['results']
+
+movies_data.each do |movie_data|
+  title = movie_data['title']
+  overview = movie_data['overview']
+  poster_path = movie_data['poster_path']
+  rating = movie_data['vote_average']
+
+  Movie.create!(
+    title: title,
+    overview: overview,
+    poster_url: "https://image.tmdb.org/t/p/w500/#{poster_path}",
+    rating: rating
+  )
+end
